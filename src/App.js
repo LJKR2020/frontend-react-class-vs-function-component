b204dccd5e56d791a550d0ba33414dcd24f97ab7
LJKR2020 <loredana.rietveld@gmail.com>
2021-12-03 13:08:26 +0100

    turned class component into hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [isLightOn, toggleIsLightOn] = useState(false);
+    function handleClick() {
+        toggleIsLightOn(!isLightOn);
+    }
   return (
     <div>
-      Schrijf hier jouw functie-component!
+      <button
+          type="button"
+          onClick={handleClick}
+      >
+          {isLightOn === true ? "Zet het licht uit" : "Zet het licht aan"}
+      </button>
     </div>
   );
 }
